c750be82ba3760303b46ed7a983186e126c6c1db
Tony Field <tony@kingsway.digital>
2025-01-25 13:21:25 -0500

    Updated information about photos from Leia.

--- src/App.tsx
+++ src/App.tsx
@@ -7,72 +7,77 @@ function App() {
   const images: DesignImage[] = [
     {
       src: 'requirements/designs/initial-views.jpeg',
-      alt: 'Initial Views',
+      alt: 'Initial Views (screen 1 is start, then go to 2 or 3 based on alliance. Next 3 screens are versions of auto screens based on what the user is doing in auto.',
       width: '100%',
     },
     {
       src: 'requirements/designs/blue-auto-start.jpeg',
-      alt: 'Blue Auto Start',
+      alt: 'AUTO: Beginning of auto (fits into above)',
       width: '100%',
     },
     {
       src: 'requirements/designs/after-start.jpeg',
-      alt: 'After Start',
+      alt: 'AUTO: Left - click score on reef; right: click score on algae',
       width: '100%',
     },
     {
       src: 'requirements/designs/general-auto-play.jpeg',
-      alt: 'General Auto Play',
+      alt: 'AUTO: Penalty options',
       width: '100%',
     },
     {
       src: 'requirements/designs/holding-both-auto.jpeg',
-      alt: 'Holding Both Auto',
+      alt: 'AUTO: Options when a user is holding both autos (shown to ensure that buttons fit)',
       width: '100%',
     },
     {
       src: 'requirements/designs/holding-teleop-with-defence.jpeg',
-      alt: 'Holding Teleop with Defence',
+      alt: 'TELEOP regular screens - showing options available based on what is being held',
       width: '100%',
     },
     {
       src: 'requirements/designs/holding-teleop.jpeg',
-      alt: 'Holding Teleop',
+      alt: 'TELEOP: once score is pressed, gives score options',
       width: '100%',
     },
     {
       src: 'requirements/designs/teleop3.jpeg',
-      alt: 'Teleop 3',
+      alt: 'TELEOP: Shows penalties for teleop AND Shows pickup coral; shows coral pickup locations (ignore "holding algae" title); shows defence timer; ',
       width: '100%',
     },
     {
       src: 'requirements/designs/endgame-comments1.jpeg',
-      alt: 'Endgame Comments 1',
+      alt: 'Endgame flow + comemnt flow',
       width: '100%',
     },
     {
       src: 'requirements/designs/endgame-comment2.jpeg',
-      alt: 'Endgame Comment 2',
+      alt: 'Endgame flow + comemnt flow (second screen of each)',
       width: '100%',
     },
     {
       src: 'requirements/designs/endgame-comments-3.jpeg',
-      alt: 'Endgame Comments 3',
+      alt: 'Endgame flow + comemnt flow (third screen of each)',
+      width: '100%',
+    },
+    {
+      src: 'requirements/designs/overall-comments.jpeg',
+      alt: 'Final human-entered comments, shows ranking points, arbitrary rating)',
       width: '100%',
     },
     {
       src: 'requirements/designs/mindmap1.jpeg',
-      alt: 'Mindmap 1',
+      alt: 'Mindmap 1 (for inspriation, not solid requirements)',
       width: '100%',
     },
     {
       src: 'requirements/designs/mindmap2.jpeg',
-      alt: 'Mindmap 2',
+      alt: 'Mindmap 2 (for inspriation, not solid requirements)',
       width: '100%',
     },
     {
       src: 'requirements/designs/mindmap-three.jpeg',
-      alt: 'Mindmap Three',
+      alt: 'Mindmap Three (for inspriation, not solid requirements)',
       width: '100%',
     },
   ];
