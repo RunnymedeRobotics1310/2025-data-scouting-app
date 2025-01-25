767b2f710b5d2717202f60e2b57b656947b47c0c
Tony Field <tony@kingsway.digital>
2025-01-24 22:39:26 -0500

    Update main app page to display shortcuts so that we can see the app designs from within the main screen.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,94 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-
+type DesignImage = {
+  src: string;
+  alt: string;
+  width: string;
+};
 function App() {
-  const [count, setCount] = useState(0);
-
+  const images: DesignImage[] = [
+    {
+      src: 'requirements/designs/initial-views.jpeg',
+      alt: 'Initial Views',
+      width: '100%',
+    },
+    {
+      src: 'requirements/designs/blue-auto-start.jpeg',
+      alt: 'Blue Auto Start',
+      width: '100%',
+    },
+    {
+      src: 'requirements/designs/after-start.jpeg',
+      alt: 'After Start',
+      width: '100%',
+    },
+    {
+      src: 'requirements/designs/general-auto-play.jpeg',
+      alt: 'General Auto Play',
+      width: '100%',
+    },
+    {
+      src: 'requirements/designs/holding-both-auto.jpeg',
+      alt: 'Holding Both Auto',
+      width: '100%',
+    },
+    {
+      src: 'requirements/designs/holding-teleop-with-defence.jpeg',
+      alt: 'Holding Teleop with Defence',
+      width: '100%',
+    },
+    {
+      src: 'requirements/designs/holding-teleop.jpeg',
+      alt: 'Holding Teleop',
+      width: '100%',
+    },
+    {
+      src: 'requirements/designs/teleop3.jpeg',
+      alt: 'Teleop 3',
+      width: '100%',
+    },
+    {
+      src: 'requirements/designs/endgame-comments1.jpeg',
+      alt: 'Endgame Comments 1',
+      width: '100%',
+    },
+    {
+      src: 'requirements/designs/endgame-comment2.jpeg',
+      alt: 'Endgame Comment 2',
+      width: '100%',
+    },
+    {
+      src: 'requirements/designs/endgame-comments-3.jpeg',
+      alt: 'Endgame Comments 3',
+      width: '100%',
+    },
+    {
+      src: 'requirements/designs/mindmap1.jpeg',
+      alt: 'Mindmap 1',
+      width: '100%',
+    },
+    {
+      src: 'requirements/designs/mindmap2.jpeg',
+      alt: 'Mindmap 2',
+      width: '100%',
+    },
+    {
+      src: 'requirements/designs/mindmap-three.jpeg',
+      alt: 'Mindmap Three',
+      width: '100%',
+    },
+  ];
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <img src="images/logo.png" alt="Runnymede Robotics" width="100%" />
+      <h1>Team 1310 Runnymede Robotics Data Scouting App</h1>
+      <p>Here are some designs of what the app might look like someday.</p>
+      <ul>
+        {images.map((image, index) => (
+          <li key={index}>
+            <h3>{image.alt}:</h3>
+            <img src={image.src} width={image.width} alt={image.alt} />
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
