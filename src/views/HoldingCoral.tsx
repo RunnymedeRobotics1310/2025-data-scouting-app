7a1a6e6f0ec28f392922183f268e09d767f2cce1
Tony Field <tony@kingsway.digital>
2025-01-29 07:14:44 -0500

    demo: score reef simple options

--- src/views/HoldingCoral.tsx
+++ src/views/HoldingCoral.tsx
@@ -1,14 +1,26 @@
 import { holding_nothing } from '../modes/holding_nothing.ts';
 import { holding_both } from '../modes/holding_both.ts';
 import { park } from '../modes/park.ts';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function HoldingCoral() {
+  const [showReefOptions, setShowReefOptions] = useState(false);
+  const navigate = useNavigate();
+
+  function scoreReef(level: number) {
+    console.log('Scored reef level ' + level);
+    navigate(holding_nothing.url);
+  }
+  function missCoral() {
+    console.log('Missed coral');
+    navigate(holding_nothing.url);
+  }
+
   return (
     <>
       <h1>Holding Coral</h1>
-      <button onClick={() => window.location.assign(holding_nothing.url)}>
-        Score Reef
-      </button>
+      <button onClick={() => setShowReefOptions(true)}>Score Reef</button>
       <button onClick={() => window.location.assign(holding_nothing.url)}>
         Drop Coral
       </button>
@@ -20,6 +32,15 @@ function HoldingCoral() {
         Endgame ---&gt;
       </button>
       <br />
+      {showReefOptions && (
+        <div>
+          <button onClick={() => missCoral()}>Oopsie</button>
+          <button onClick={() => scoreReef(1)}>1</button>
+          <button onClick={() => scoreReef(2)}>2</button>
+          <button onClick={() => scoreReef(3)}>3</button>
+          <button onClick={() => scoreReef(4)}>4</button>
+        </div>
+      )}
       <img
         src={'requirements/screens/holding-coral.jpeg'}
         width={'25%'}
