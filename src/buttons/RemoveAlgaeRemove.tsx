f8761d9aaca2bf6c4364de31e014eced52817c8a
Tony Field <tony@kingsway.digital>
2025-03-05 23:24:48 -0500

    When algae is removed, dim button for 1.25s so that users can tell that something happened. (#29)

--- src/buttons/RemoveAlgaeRemove.tsx
+++ src/buttons/RemoveAlgaeRemove.tsx
@@ -2,6 +2,7 @@ import { Mode } from '../common/mode.ts';
 import { removeAlgae } from '../functions/removeAlgae.ts';
 import { getScoutingSessionId } from '../storage/util.ts';
 import Loading from '../common/Loading.tsx';
+import { useEffect, useState } from 'react';
 
 type PropTypes = {
   mode: Mode;
@@ -11,12 +12,25 @@ function RemoveAlgaeRemove(props: PropTypes) {
   const mode = props.mode;
   const cb = props.clearCallback;
   const scoutingSessionId = getScoutingSessionId();
+  const [showRemoved, setShowRemoved] = useState(false);
+
+  useEffect(() => {
+    if (showRemoved) {
+      const toRef = setTimeout(() => {
+        setShowRemoved(false);
+        clearTimeout(toRef);
+      }, 1250);
+    }
+  }, [showRemoved]);
+
   if (!scoutingSessionId) return <Loading />;
+  if (showRemoved) return <button disabled>Remove</button>;
   return (
     <button
       onClick={() => {
         if (cb) cb();
         removeAlgae(scoutingSessionId, mode, false);
+        setShowRemoved(true);
       }}
     >
       Remove
