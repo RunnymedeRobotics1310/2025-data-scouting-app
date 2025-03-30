71bf3652779855e2d692057b2fd92147e5034f96
Tony Field <tony@kingsway.digital>
2025-03-29 23:26:37 -0400

    Finish downloading schedule data.

--- src/views/Sync.tsx
+++ src/views/Sync.tsx
@@ -1,18 +1,23 @@
 import { isDevelopment } from '../dev/util.ts';
 import RBSyncMain from './sub/RBSyncMain.tsx';
+import SyncCount from './sub/SyncCount.tsx';
+import DownloadTournamentsAndSchedule from './sub/DownloadTournamentsAndSchedule.tsx';
+import { useNavigate } from 'react-router-dom';
 
 export default function Sync() {
+  const navigate = useNavigate();
   return (
     <>
       {/*<SyncGoogleApiLoader />*/}
       {isDevelopment() && (
         <>
           <hr />
-          <h2>Synchronize Data to Raven Brain</h2>
-          <p>
-            This prototype is currently hidden behind the isDevelopment() check.
-          </p>
+          <h2>Tournament & Schedule Data</h2>
+          <SyncCount />
+
+          <DownloadTournamentsAndSchedule />
           <RBSyncMain />
+          <button onClick={() => navigate('/')}>Return Home</button>
         </>
       )}
     </>
--- src/views/sub/RBSyncMain.tsx
+++ src/views/sub/RBSyncMain.tsx
@@ -1,124 +1,17 @@
-import SyncCount from './SyncCount.tsx';
 import Spinner from '../../common/Spinner.tsx';
-import { useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import {
-  useScheduleDetail,
-  useTournamentList,
-} from '../../storage/ravenbrain.ts';
-import { Tournament } from '../../types/Tournament.ts';
+import { useState } from 'react';
 
 function RBSyncMain() {
-  const navigate = useNavigate();
   const [content, setContent] = useState('');
   const [syncing, setSyncing] = useState(false);
 
-  const [loadTournaments, setLoadTournaments] = useState(false);
-  function handleDownloadClick() {
-    setLoadTournaments(true);
-  }
-
-  const [doSync, setDoSync] = useState(false);
   function handleSyncClick() {
     console.log("Help me, I'm syncing!");
   }
-  /*
-   * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-   *
-   *
-   *                                 DISPLAY
-   *
-   *
-   * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-   */
 
-  function LoadingTournaments() {
-    const { list, error, loading } = useTournamentList();
-
-    useEffect(() => {
-      if (list && list.length > 0) {
-        const tournaments: Tournament[] = [];
-        list.forEach(t => {
-          tournaments.push({
-            id: t.id,
-            name: t.name,
-            startDate: new Date(t.startTime),
-            scheduleGoogleSheetId: '-',
-            eventLogGoogleSheetId: '-',
-          });
-        });
-        localStorage.setItem('rrAllTournaments', JSON.stringify(tournaments));
-      }
-    }, [list]);
-
-    if (loading) {
-      return (
-        <div>
-          <Spinner />
-          Loading tournament list...
-        </div>
-      );
-    }
-    if (error) {
-      return <div>Error loading tournaments: {error}</div>;
-    }
-
-    return (
-      <div>
-        <p>Tournament List Updated</p>
-        {list.map(tournament => (
-          <LoadSchedule key={tournament.id} tournament={tournament} />
-        ))}
-      </div>
-    );
-  }
-  type LoadScheduleProps = {
-    tournament: any;
-  };
-  function LoadSchedule(props: LoadScheduleProps) {
-    const { tournament } = props;
-    const { schedule, error, loading } = useScheduleDetail(tournament.id);
-    if (loading) {
-      return (
-        <div>
-          <Spinner />
-          Loading schedule for {tournament.name}...
-        </div>
-      );
-    }
-    if (error) {
-      return (
-        <div>
-          Error loading schedule for {tournament.name}: {error}
-        </div>
-      );
-    }
-    console.log('Save schedule data here!', schedule); // todo: fixme; save data to local repo
-    return <div>Schedule for {tournament.name} loaded</div>;
-  }
   return (
     <div>
-      <h2>Tournament & Schedule Data</h2>
-      <SyncCount />
-      {syncing && (
-        <div>
-          <Spinner />
-          Syncing...
-        </div>
-      )}
-
-      <p>
-        Download tournament and schedule data before scouting matches. This only
-        needs to be done once per tournament. Schedule data for all tournaments
-        will be downloaded every time.
-      </p>
-      {loadTournaments ? (
-        <LoadingTournaments />
-      ) : (
-        <button onClick={() => handleDownloadClick()}>
-          Download Tournament & Schedule Data
-        </button>
-      )}
+      <h3>Sync Scouting Data</h3>
       <p>
         Synchronize your data with RavenBrain. Your data is stored locally on
         your browser, so it is not necessary to sync after each match. However,
@@ -127,9 +20,14 @@ function RBSyncMain() {
         internet with your device and press the sync button. You'll have to
         enter the password provided to you by the team.
       </p>
+      {syncing && (
+        <div>
+          <Spinner />
+          Syncing...
+        </div>
+      )}
       <button onClick={() => handleSyncClick()}>Sync Scouting Data</button>
       <p>&nbsp;</p>
-      <button onClick={() => navigate('/')}>Return Home</button>
       <h4>Sync Status Messages</h4>
       <pre id="content" className="googleExampleContentPreStyle">
         {content}
